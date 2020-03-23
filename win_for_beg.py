58054504dfe4bced4cb4341fc71d085ebb30d73c
Wollermann <53054305+Wollermann@users.noreply.github.com>
2020-03-23 15:22:16 +0300

    Исправление багов, добавление директорий

--- win_for_beg.py
+++ win_for_beg.py
@@ -11,26 +11,32 @@ while True:
     request = input('Введите команду: ')
 
     #Команда 'Открой'
-    if request[0:request.find(' ')].lower() == 'открой':
-        prog1 = request[request.find(' ') + 1:]
-        prog2 = unidecode(request[request.find(' ') + 1:])
-        #Проверяем в каждой директории
-        for dir in dirs:
-            #Находим расширение файла
-            for obj in os.listdir(dir):
-                if os.path.splitext(obj)[0] == prog1 or os.path.splitext(obj)[0] == prog2:
-                    exp = os.path.splitext(obj)[1]
+    if request[:request.find(' ')].lower() == 'открой':
+        #Если команда "открой сайт"
+        if request[request.find(' ')+1:].lower() == "сайт":
+            url = input("Укажите адрес страницы: ")
+            webbrowser.open(url)
+        #Открывает программу в иных случаях
+        else:
+            prog1 = request[request.find(' ') + 1:]
+            prog2 = unidecode(request[request.find(' ') + 1:])
+            #Проверяем в каждой директории
+            for dir in dirs:
+                #Находим расширение файла
+                for obj in os.listdir(dir):
+                    if os.path.splitext(obj)[0] == prog1 or os.path.splitext(obj)[0] == prog2:
+                        exp = os.path.splitext(obj)[1]
+                        break
+                #Запускаем, если файл существует. ТРЕБУЕТ БОЛЬШОЙ ОПТИМИЗАЦИИ
+                if f'{prog1}{exp}' in os.listdir(dir) or f'{prog2}{exp}' in os.listdir(dir):
+                    try:
+                        os.startfile(f'{dir}{prog1}{exp}')
+                    except:
+                        os.startfile(f'{dir}{prog2}{exp}')
                     break
-            #Запускаем, если файл существует. ТРЕБУЕТ БОЛЬШОЙ ОПТИМИЗАЦИИ
-            if f'{prog1}{exp}' in os.listdir(dir) or f'{prog2}{exp}' in os.listdir(dir):
-                try:
-                    os.startfile(f'{dir}{prog1}{exp}')
-                except:
-                    os.startfile(f'{dir}{prog2}{exp}')
-                break
 
     #команда "горячие клавиши"
-    elif request[:request.find(' ')].lower() == "горячие":
+    elif request.lower() == "горячие клавиши":
         print("Список горячих клавиш для Windows 10: ",
             "ctrl + C - скопировать текст",
             "ctrl + V - вставить скопированный текст",
@@ -41,14 +47,19 @@ while True:
             "Больше информации вы можете узнать тут: https://g-ek.com/sochetaniya-klavish-dlya-kalkulyatora-v-windows-10",
             sep="\n")
 
-    #команда для открытия веб-страниц(браузер=default)
-    elif request.lower() == "открой сайт":
-        url = input("Укажите адрес страницы: ")
-        webbrowser.open(url)
 
     #команда для поиска в интернете(браузер=default)
-    elif request[:request.find(' ')].lower() == "поиск":
+    elif request.lower() == "поиск":
         webbrowser.open("https://www.google.ru/")
+
+    #добавление директории. В будущем можно сделать добавление чего либо другого.
+    elif request[:request.find(' ')].lower() == 'добавить':
+        if request[request.find(' ')+1:] == 'директорию':
+            newdir = input('Введите новую директорию: ')
+            dirs.append(newdir)
+
+
+
 
     #Команда 'Помощь' (по командам бота)
     elif request.lower() == 'помощь':
